5e38ae33e170629e3caa4903a7d304950f02e6de
TT2O <tomonari3011@gmail.com>
2025-11-27 15:45:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,18 @@ st.write(score)
 
 st.radio("性別を選択してください",["男性","女性"])
 
+list = [{"latitude":35.05, "longitude":135.76},#デフォルト現在地
+        {"latitude":35.04, "longitude":135.75},#紫明小学校
+       ]
+st.map(list)
+
 camera_photo = st.camera_input("写真を撮影します")
 if camera_photo:
   st.image(camera, caption="写真", use_column_width=True)
+
+#テスト
+"""
+img = 
+st.import(cv2)
+"""
+
