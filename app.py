5caefee2dbc23471fae9ff19d29b41eb9e44de2f
TT2O <tomonari3011@gmail.com>
2025-11-27 15:12:42 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("こんにちは、吉村ゼミ")
+
+name = st.text_input("名前を入力してください")
+
+st.write(name)
+
+camera_photo = st.camera.input("写真を撮影します")
+if camera_photo:
+  st.image(camera, caption="写真", use_column_width=True)
