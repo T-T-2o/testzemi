3e590d4521ee72559afc95d9700f6b268cad1b81
TT2O <tomonari3011@gmail.com>
2025-11-27 15:29:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ st.checkbox("同意します")
 address = st.selectbox("次の中から現住所を教えてください",["大阪府","京都府","滋賀県"])
 st.write(address)
 
+hobby = st.multiselect("趣味を次から選択してください"["映画","読書","音楽","運動"])
+st.write(hobby)
+
 camera_photo = st.camera_input("写真を撮影します")
 if camera_photo:
   st.image(camera, caption="写真", use_column_width=True)
