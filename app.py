824d939394853de4a1ef3abaccd300f1c79c98c2
TT2O <tomonari3011@gmail.com>
2025-11-27 15:20:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ name = st.text_input("名前を入力してください")
 
 st.write(name)
 
+st.checkbox("同意します")
+
 camera_photo = st.camera.input("写真を撮影します")
 if camera_photo:
   st.image(camera, caption="写真", use_column_width=True)
