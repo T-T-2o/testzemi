8983b626657b1248a4937b2a0088e984b2822807
TT2O <tomonari3011@gmail.com>
2025-11-27 15:23:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ name = st.text_input("名前を入力してください")
 st.write(name)
 
 st.checkbox("同意します")
+st.selectbox("次の中から現住所を教えてください",["大阪府","京都府","滋賀県")
 
 camera_photo = st.camera.input("写真を撮影します")
 if camera_photo:
