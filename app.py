7317cdc06bb0ecd67ade0be4ccf42db8fbfb9fda
TT2O <tomonari3011@gmail.com>
2025-12-19 09:13:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,17 @@ COLORS = [
     "Red"
 ]
 
+COLOR_RGB = {
+    "Black": (30, 30, 30),
+    "White": (240, 240, 240),
+    "Gray": (160, 160, 160),
+    "Navy": (40, 60, 100),
+    "Brown": (120, 80, 50),
+    "Beige": (210, 200, 170),
+    "Green": (60, 120, 80),
+    "Red": (160, 50, 50)
+}
+
 # -----------------------------
 # 2. User Input
 # -----------------------------
@@ -119,16 +130,35 @@ def generate_outfit(genre, color):
 # -----------------------------
 
 def generate_image(outfit):
-    img = Image.new("RGB", (240, 420), "white")
+    base_color = COLOR_RGB[outfit["Color Theme"]]
+
+    img = Image.new("RGB", (260, 440), (255, 255, 255))
     d = ImageDraw.Draw(img)
 
-    d.rectangle([80, 40, 160, 140], outline="black", width=3)   # Head
-    d.rectangle([60, 140, 180, 250], outline="black", width=3) # Outer
-    d.rectangle([75, 155, 165, 235], outline="gray", width=2)  # Inner
-    d.rectangle([75, 250, 165, 390], outline="black", width=3) # Bottom
+    # Head
+    d.ellipse([100, 20, 160, 80], fill=(220, 200, 180))
 
-    d.text((10, 10), outfit["Color Theme"], fill="black")
+    # Outer (Jacket / Hoodie)
+    d.rectangle([60, 100, 200, 260], fill=base_color, outline="black", width=3)
+
+    # Hoodie hood
+    if "Hoodie" in outfit["Outer"]:
+        d.arc([80, 80, 180, 140], start=0, end=180, fill="black", width=4)
+
+    # Inner (Tee / Shirt)
+    inner_color = tuple(min(255, c + 40) for c in base_color)
+    d.rectangle([80, 120, 180, 240], fill=inner_color, outline="black", width=2)
+
+    # Graphic Tee logo
+    if "Graphic Tee" in outfit["Inner"]:
+        d.rectangle([110, 160, 150, 200], fill=(255, 255, 255))
+
+    # Bottom
+    bottom_color = tuple(max(0, c - 40) for c in base_color)
+    d.rectangle([90, 260, 170, 400], fill=bottom_color, outline="black", width=3)
+
     return img
+
 
 # -----------------------------
 # 8. Generate 3 Outfits (Color Duplication Avoidance)
